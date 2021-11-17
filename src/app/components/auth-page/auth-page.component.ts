4ea8fffa394d0f4e1309654f2b10b01b084a6211
f0msy <fomsy29@gmail.com>
2021-11-17 05:16:46 +0300

    chore: wip

---
+++ src/app/components/auth-page/auth-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'auth-page',
+  templateUrl: 'auth-page.component.html',
+  styleUrls: ['./auth-page.component.scss'],
+})
+export class AuthComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
